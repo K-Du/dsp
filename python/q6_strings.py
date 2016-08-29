4c7e142c86fd39548ea97d4e350e3fc77925cb0f
Kevin Du <kevinkldu@gmail.com>
2016-08-28 21:18:32 -0700

    Completed Q6.

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -18,8 +18,7 @@ def donuts(count):
     >>> donuts(99)
     'Number of donuts: many'
     """
-    raise NotImplementedError
-
+    return 'Number of donuts: {}'.format(count) if count < 10 else 'Number of donuts: many'
 
 def both_ends(s):
     """
@@ -37,7 +36,10 @@ def both_ends(s):
     >>> both_ends('xyz')
     'xyyz'
     """
-    raise NotImplementedError
+    if len(s) < 2:
+        return ''
+    else: 
+        return s[:2]+s[-2:]
 
 
 def fix_start(s):
@@ -56,8 +58,7 @@ def fix_start(s):
     >>> fix_start('donut')
     'donut'
     """
-    raise NotImplementedError
-
+    return s[0] + s[1:].replace(s[0], '*')
 
 def mix_up(a, b):
     """
@@ -74,7 +75,7 @@ def mix_up(a, b):
     >>> mix_up('pezzy', 'firm')
     'fizzy perm'
     """
-    raise NotImplementedError
+    return b[0:2] + a[2:] + ' ' + a[0:2] + b[2:] 
 
 
 def verbing(s):
@@ -91,8 +92,12 @@ def verbing(s):
     >>> verbing('do')
     'do'
     """
-    raise NotImplementedError
-
+    if len(s) < 3:
+        return s
+    elif s[-3:] == 'ing':
+        return s + 'ly'
+    else:
+        return s + 'ing'
 
 def not_bad(s):
     """
@@ -111,8 +116,10 @@ def not_bad(s):
     >>> not_bad("It's bad yet not")
     "It's bad yet not"
     """
-    raise NotImplementedError
-
+    if s.find('not') < s.find('bad'):
+        return s[:s.find('not')] + 'good' + s[s.find('bad')+3:]
+    else:
+        return s
 
 def front_back(a, b):
     """
@@ -130,4 +137,4 @@ def front_back(a, b):
     >>> front_back('Kitten', 'Donut')
     'KitDontenut'
     """
-    raise NotImplementedError
+    return a[:int(round(len(a)/2.))] + b[:int(round(len(b)/2.))] + a[int(round(len(a)/2.)):] + b[int(round(len(b)/2.)):]
