7cbcec11f143ef20d3ccb27d9632f7d03f0f9680
Kevin Du <kevinkldu@gmail.com>
2016-09-02 23:01:19 -0700

    Completed

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,7 @@
-PLACE YOUR CODE HERE
+import urllib2
+import pandas as pd
+
+faculty_url = urllib2.urlopen('https://raw.githubusercontent.com/K-Du/dsp/master/python/faculty.csv')
+faculty_csv = pd.read_csv(faculty_url, skipinitialspace = True)
+
+faculty_csv.email.to_csv('emails.csv', index = False)
